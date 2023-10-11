92afa9e23f7d0029623c574dba64a35611d085e2
matin-mehdipour <matin.mehdipour@outlook.com>
2023-10-11 00:36:30 -0700

    can run in a loop with n seconds between requests and  present status and headers of response

--- do-loop-script.ps1
+++ do-loop-script.ps1
@@ -3,15 +3,34 @@ $url = "https://matintest.egnyte.com/v5/settings-ed"
 
 # Define the headers you want to include in the request
 $headers = @{
-    "Authorization" = "OAUTH oauth_token=3700930f-865b-4884-a3cc-1617fd60be1c,oauth_version=2.0"
-    "Content-Type" = "application/json"
-    "User-Agent" = "Egnyte/3.15.1.2"
-    "Egnyte-Client-Revision" = "ELC/167241"
+    "Authorization"           = "OAUTH oauth_token=3700930f-865b-4884-a3cc-1617fd60be1c,oauth_version=2.0"
+    "Content-Type"            = "application/json"
+    "User-Agent"              = "Egnyte/3.15.1.2"
+    "Egnyte-Client-Revision"  = "ELC/167241"
     "X-Egnyte-Action-Sources" = "EGD"
 }
 
-$response = Invoke-WebRequest -Uri $url -Headers $headers -Method Get
+$count = 0
 
-Write-Host $response.headers
-Write-Host "---------------------------------------------------"
-Write-Host $response.StatusCode
+while ($true)
+{
+    try
+    {
+        $count++
+        Write-Host "-------------------Run $($count)--------------------------------"
+        $response = Invoke-WebRequest -Uri $url -Headers $headers -Method Get
+
+        Write-Host "Response Headers: "  $response.headers
+        Write-Host "---------------------------------------------------"
+        Write-Host "Response Status Code " $response.StatusCode
+        Write-Host "---------------------------------------------------`r`n"
+        Start-Sleep -Seconds 2
+    }
+    catch
+    {
+        Write-Host "An error occurred: $($_.Exception.Message)"
+    }
+    
+}
+
+
