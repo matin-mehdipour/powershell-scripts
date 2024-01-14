50549c1ffef281eac377ed7f9dd71c5201301ea5
Matin Mehdipour <matin@Matins-MBP.attlocal.net>
2024-01-13 17:27:46 -0800

    can search for descriptions now in cli

--- chase-activity-parser/script.ps1
+++ chase-activity-parser/script.ps1
@@ -5,6 +5,41 @@ $filepath = "$currentDirectory/$fileName"
 
 $inputFile = Import-Csv -Path $filepath -Delimiter ","
 
-foreach ($line in $inputFile) {
-    $line
+# Create an ArrayList
+$rows = New-Object System.Collections.ArrayList
+
+function getMatchingRows($stringMatch)
+{
+    $matchingRows = @()
+
+    foreach ($row in $rows)
+    {
+        if ($row.Description -imatch $stringMatch)
+        {
+            $matchingRows += $row
+        }
+    }
+
+    return $matchingRows
 }
+
+
+foreach ($line in $inputFile) 
+{
+    $rows.Add($line) > $null
+}
+
+Write-Host "finished reading file"
+
+while ($true)
+{
+    Write-Host "entered while"
+    $userInput = Read-Host "Enter a command"
+    $matchingRows = getMatchingRows($userInput)
+
+    $matchingRows
+    
+
+  
+}
+
