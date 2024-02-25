7456f112bf3bb58a28205e66ef02c0dca5aa3449
Matin Mehdipour <mmehdipour@matinhome.app>
2024-02-25 15:26:20 -0800

    can get arg and do based on mode - store apps in list and get ids in loop for the processes before killing them

--- workmode-switch/script.ps1
+++ workmode-switch/script.ps1
@@ -1,3 +1,19 @@
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$Arguments
+)
+
+# Create an ArrayList
+$appList = New-Object System.Collections.ArrayList
+
+$appList.Add("chrome")
+$appList.Add("slack")
+$appList.Add("8x8 Work")
+
+$processIDs = New-Object System.Collections.ArrayList
+
+
 param(
     [Parameter(Mandatory=$true)]
     [string[]]$Arguments
@@ -6,3 +22,23 @@ param(
 # Access individual arguments
 $mode = $Arguments[0]
 
+if ($mode -eq "wm") 
+{
+    # Do something
+}
+
+elif ($mode -eq "nm")
+{
+ 
+    foreach ($app in $appList)
+    {
+        $process = Get-Process -Name $app -ErrorAction SilentlyContinue
+        if ($process -ne $null)
+        {
+            $processIDs.Add($process.Id)
+        }
+    }
+
+
+}
+    
