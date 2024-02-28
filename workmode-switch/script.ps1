18487a036a180436feeb1138c37a12b6e86da479
Matin Mehdipour <mmehdipour@matinhome.app>
2024-02-28 08:12:01 -0800

    added dns switcher script

--- workmode-switch/script.ps1
+++ workmode-switch/script.ps1
@@ -4,10 +4,16 @@
 # Create an ArrayList
 $appList = New-Object System.Collections.ArrayList
 
-$appList.Add("chrome")
-$appList.Add("slack")
-$appList.Add("8x8 Work")
-$appList.Add("zoom")
+$userProfilePath = $env:USERPROFILE
+
+
+$appMap = @{
+    "chrome" = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    "slack" = "%userprofile%\AppData\Local\slack\slack.exe"
+    "8x8 Work" = "C:\Program Files (x86)\8x8 Virtual Office\8x8 Virtual Office.exe"
+    "zoom" = "%userprofile%\AppData\Roaming\Zoom\bin\Zoom.exe"
+}
+
 
 $processIDs = New-Object System.Collections.ArrayList
 
@@ -18,19 +24,19 @@ $mode = $args[0]
 
 if ($mode -eq "wm") 
 {
-    # Do something
+   foreach ($app in $appMap.Keys)
+   {
+       $process = Start-Process -FilePath $appMap[$app] -PassThru
+       $processIDs.Add($process.Id)
+   }
 }
 
 elseif ($mode -eq "nm")
 {
  
-    foreach ($app in $appList)
+    foreach ($app in $appMap.Keys)
     {
-        $process = Get-Process -Name $app -ErrorAction SilentlyContinue
-        if ($process -ne $null)
-        {
-            $processIDs.Add($process.Id)
-        }
+        $appList.Add($app)
     }
     foreach ($id in $processIDs)
     {
