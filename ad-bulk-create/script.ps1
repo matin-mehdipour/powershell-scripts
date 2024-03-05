4eb649b7bb4cd4e852b374996c1ae018bfc5938f
Matin Mehdipour <matin.mehdipour@outlook.com>
2024-03-05 00:23:35 -0800

    can parse input file and add user

--- ad-bulk-create/script.ps1
+++ ad-bulk-create/script.ps1
@@ -1,7 +1,43 @@
 
+$inputPath = ''
+
+
 function Main 
 {
-   
+    $inputPath = $args[1]
+
+    if ($inputPath -eq '') 
+    {
+        Write-Host "Please provide a path to the file"
+        return
+    }
+}
+function parseInput()
+{
+    $inputLines = Get-Content $inputPath
+
+    foreach ($line in $inputLines)
+    {
+       $name = $line.Split(',')
+       $username = $name[0].Substring(0,1) + $name[1]
+    }
+}
+function addUser($username, $name)
+{
+    $username = $username.ToLower()
+
+    $userParams = @{
+        SamAccountName  = $username
+        UserPrincipalName = $username + "@matinhome.app"
+        Name            = $name[0] + " " + $name[1]
+        GivenName       = $name[0]
+        Surname         = $name[1]
+        DisplayName     = $name[0] + " " + $name[1]
+        Enabled         = $true
+        AccountPassword = (ConvertTo-SecureString -AsPlainText "Support123" -Force)
+    }
+
+    New-ADUser @userParams
 }
 
 Main
