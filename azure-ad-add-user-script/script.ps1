742afb90140c813051fb7e33c70ab6f797a3a0f4
Matin Mehdipour <matin.mehdipour@outlook.com>
2024-03-23 06:38:19 -0700

    added basic command to create user in azure

--- azure-ad-add-user-script/script.ps1
+++ azure-ad-add-user-script/script.ps1
@@ -2,30 +2,46 @@
 
 function Main 
 {
-    if (Get-AzContext -ErrorAction SilentlyContinue) {
+    if (Get-AzContext -ErrorAction SilentlyContinue)
+    {
         Write-Host "Connected to Azure"
-    } else {
+        exit
+    }
+    else
+    {
         Write-Host "Not connected to Azure"
         Read-Host "Press Enter to connect to Azure"
-        #Connect-AzAccount
+        if (Get-Process -Name Connect-AzAccount -ErrorAction SilentlyContinue)
+        {
+            # If it's still running, stop the process
+            Stop-Process -Name Connect-AzAccount -Force
+        }       
+        $result = Connect-AzAccount
+        Write-Host $result
+        exit
     }
     #Connect-AzAccount
 
-    $fileContent = Get-Content -Path "C:\Users\mmehdipour\github-projects\powershell-scripts\azure-ad-add-user-script\input.csv"
+    $fileContent = Get-Content -Path "input.csv"
     foreach ($line in $fileContent)
     {   
-        Write-Host "Processing line: $line"
+        #Write-Host "Processing line: $line"
         $lineArray = $line -split ","
         $email = $lineArray[0]
         $name = $lineArray[1]
 
         Write-Host "Email: $email"
         #Write-Host "Name: $name"
-
+        $name = $name.Trim()
         $nameSplit = $name -split " "
 
-        Write-Host "First Name: $($nameSplit[0]) Last Name: $($nameSplit[1])"
+        $displayName = "John Doe"
+        $userPrincipalName = "johndoe@example.com"
+        $password = "YourStrongPasswordHere"
+        $accountEnabled = $true
 
+        # Create the new user
+        New-AzureADUser -DisplayName $displayName -UserPrincipalName $userPrincipalName -AccountEnabled $accountEnabled -Password $password
     }
 }
 
