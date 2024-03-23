c818cf0f6dc06154549133856f7e9d629a229841
Matin Mehdipour <mmehdipour@matinhome.app>
2024-03-23 04:30:46 -0700

    can parse csv and split username

--- azure-ad-add-user-script/script.ps1
+++ azure-ad-add-user-script/script.ps1
@@ -2,7 +2,30 @@
 
 function Main 
 {
-    
+    if (Get-AzContext -ErrorAction SilentlyContinue) {
+        Write-Host "Connected to Azure"
+    } else {
+        Write-Host "Not connected to Azure"
+        Read-Host "Press Enter to connect to Azure"
+        #Connect-AzAccount
+    }
+    #Connect-AzAccount
+
+    $fileContent = Get-Content -Path "input.csv"
+    foreach ($line in $fileContent)
+    {   
+        Write-Host "Processing line: $line"
+        $lineArray = $line -split ","
+        $email = $lineArray[0]
+        $name = $lineArray[1]
+
+        Write-Host "Email: $email"
+        Write-Host "Name: $name"
+
+        $nameSplit = $name -split " "
+
+        
+    }
 }
 
 Main
